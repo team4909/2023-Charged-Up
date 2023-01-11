fb1687c083cd9b8da94422a65cfa8ad47d2678bd
mpatankar6 <mpatankar06@gmail.com>
2023-01-10 21:24:18 -0500

    Add controller implementation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,8 +6,15 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
 
 public class RobotContainer {
+
+  private final CommandXboxController m_driverController = new CommandXboxController(0);
+  private final CommandXboxController m_operatorController = new CommandXboxController(1);
+
+
   public RobotContainer() {
     configureBindings();
   }
@@ -16,5 +23,23 @@ public class RobotContainer {
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
+  }
+
+  private double deadband(double value, double deadband) {
+    if (Math.abs(value) > deadband) {
+      if (value > 0d)
+        return (value - deadband) / (1d - deadband);
+      else
+        return (value + deadband) / (1d - deadband);
+    } else {
+      return 0d;
+    }
+  }
+
+  private double modifyAxis(double value) {
+    value = deadband(value, 0.05);
+    // Square the axis
+    value = Math.copySign(value * value, value);
+    return value;
   }
 }
