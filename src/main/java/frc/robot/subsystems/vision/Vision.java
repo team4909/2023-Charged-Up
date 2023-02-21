765c2078d5cae4d5938ce9e00a538b562a5065fe
mpatankar6 <mpatankar06@gmail.com>
2023-02-21 14:03:53 -0500

    add pose3d parser

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -1,10 +1,17 @@
 package frc.robot.subsystems.vision;
 
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.limelight.LimelightHelpers;
+import frc.lib.limelight.LimelightHelpers.LimelightResults;
 
 public class Vision extends SubsystemBase {
 
     private static Vision m_instance = null;
+    private LimelightResults m_visionResults;
 
     private Vision() {
 
@@ -12,6 +19,8 @@ public class Vision extends SubsystemBase {
 
     @Override
     public void periodic() {
+        m_visionResults = LimelightHelpers.getLatestResults("limelight");
+        SmartDashboard.putString("Vision Pose", this.generatePose().toString());
     }
 
     public static Vision getInstance() {
@@ -19,6 +28,13 @@ public class Vision extends SubsystemBase {
             m_instance = new Vision();
         }
         return m_instance;
+    }
+
+    private Pose3d generatePose() {
+        double[] poseCoords = m_visionResults.targetingResults.botpose;
+        return new Pose3d(
+                new Translation3d(poseCoords[0], poseCoords[1], poseCoords[2]),
+                new Rotation3d(poseCoords[3], poseCoords[4], poseCoords[5]));
     }
 
 }
