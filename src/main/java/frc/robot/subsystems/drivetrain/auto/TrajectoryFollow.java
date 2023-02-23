ce1a92da2cc2b602b0f57c46bcb9d2aec3615c8f
mpatankar6 <mpatankar06@gmail.com>
2023-02-23 11:18:24 -0500

    drop in pose estimator in place of odometry

--- src/main/java/frc/robot/subsystems/drivetrain/auto/TrajectoryFollow.java
+++ src/main/java/frc/robot/subsystems/drivetrain/auto/TrajectoryFollow.java
@@ -20,18 +20,22 @@ public class TrajectoryFollow extends CommandBase {
     private double m_timeout = DrivetrainConstants.DEFAULT_TIMEOUT;
     private final Timer timer = new Timer();
     private final Drivetrain m_drivetrain = Drivetrain.getInstance();
+    private final boolean m_isFirstPath;
 
     public TrajectoryFollow() {
         m_pathName = "Stay Still";
+        m_isFirstPath = true;
     }
 
-    public TrajectoryFollow(String pathName) {
+    public TrajectoryFollow(String pathName, boolean isFirstPath) {
         m_pathName = pathName;
+        m_isFirstPath = isFirstPath;
     }
 
-    public TrajectoryFollow(Pair<String, Double> traj) {
+    public TrajectoryFollow(Pair<String, Double> traj, boolean isFirstPath) {
         m_pathName = traj.getFirst();
         m_timeout = traj.getSecond();
+        m_isFirstPath = isFirstPath;
     }
 
     @Override
@@ -45,6 +49,8 @@ public class TrajectoryFollow extends CommandBase {
         }
 
         m_drivetrain.setFieldTrajectory(m_trajectory);
+        if (m_isFirstPath)
+            m_drivetrain.resetPose(m_trajectory.getInitialHolonomicPose());
 
         new PPSwerveControllerCommand(
                 m_trajectory,
